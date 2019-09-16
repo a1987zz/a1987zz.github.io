f5da0b930850cc0b0870a016bffdaa785b2ff0ee
a1987zz <a1987zz@mail.ru>
2019-09-16 06:33:27 +0300

    Add files via upload

---
+++ v1/docs/static/content.js
@@ -0,0 +1,11 @@
+﻿(function() {
+  var src = document.getElementsByTagName('script')[0].src.replace('/content.js', '/source/');
+  
+  function include(name) {
+    document.writeln('<script src="'+src+name+'" type="text/javascript"></script>');
+  }
+  include('jquery.js'), include('tree.jquery.js'), include('sidebar.js'), include('connent.js')
+  include('data_toc.js'), include('data_index.js'), include('data_translate.js'), include('main.js'), include('mousetrap.js'), include('prism.js'), include('bindkeys.js')
+})();
+
+    document.writeln('<link href="../static/prism.css" rel="stylesheet" type="text/css"/>');
