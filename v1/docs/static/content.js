95eb6e85bc279193e9e5a4231689cfd66038556d
a1987zz <a1987zz@mail.ru>
2019-09-16 03:38:56 +0300

    Update content.js

--- v1/docs/static/content.js
+++ v1/docs/static/content.js
@@ -3,7 +3,6 @@
   
   function include(name) {
     document.writeln('<script src="'+src+name+'" type="text/javascript"></script>');
-    document.writeln('<link href="../static/prism.css" rel="stylesheet" type="text/css"/>');
   }
   include('jquery.js'), include('tree.jquery.js'), include('sidebar.js'), include('connent.js')
   include('data_toc.js'), include('data_index.js'), include('data_translate.js'), include('main.js'), include('mousetrap.js'), include('prism.js'), include('bindkeys.js')
